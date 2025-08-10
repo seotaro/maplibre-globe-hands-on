df9c9a6df8978379fe82893dd31ec7d9cc16fd27
seotaro <mail2017@seotaro.com>
2025-08-10 09:57:04 +0900

    add image layer

--- src/components/hooks/useMap.js
+++ src/components/hooks/useMap.js
@@ -31,6 +31,75 @@ export const useMap = (options = {}) => {
         type: 'globe',
       });
 
+      // イメージをマッピングする
+      {
+        map.current.addSource('image1', {
+          type: 'image',
+          url: 'image.png',
+          coordinates: [
+            [135.0, 40.0], // left top     : west north
+            [145.0, 40.0], // right top    : east north
+            [145.0, 35.0], // right bottom : east south
+            [135.0, 35.0]  // left bottom  : west south
+          ]
+        });
+
+        map.current.addLayer({
+          id: 'image1-layer',
+          type: 'raster',
+          source: 'image1',
+          paint: {
+            'raster-opacity': 0.8
+          }
+        });
+      }
+
+      // イメージをマッピングする
+      {
+        map.current.addSource('image2', {
+          type: 'image',
+          url: 'image.png',
+          coordinates: [
+            [135.0, 80.0], // left top     : west north
+            [145.0, 80.0], // right top    : east north
+            [145.0, 70.0], // right bottom : east south
+            [135.0, 70.0]  // left bottom  : west south
+          ]
+        });
+
+        map.current.addLayer({
+          id: 'image2-layer',
+          type: 'raster',
+          source: 'image2',
+          paint: {
+            'raster-opacity': 0.8
+          }
+        });
+      }
+
+
+      // イメージをマッピングする
+      {
+        map.current.addSource('image3', {
+          type: 'image',
+          url: 'image.png',
+          coordinates: [
+            [135.0, 60.0], // left top     : west north
+            [145.0, 60.0], // right top    : east north
+            [145.0, 50.0], // right bottom : east south
+            [135.0, 50.0]  // left bottom  : west south
+          ]
+        });
+
+        map.current.addLayer({
+          id: 'image3-layer',
+          type: 'raster',
+          source: 'image3',
+          paint: {
+            'raster-opacity': 0.8
+          }
+        });
+      }
     });
 
     // クリーンアップ
